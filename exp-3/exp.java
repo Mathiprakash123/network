0ab998422454e92dcf8f3a007c25b1967235bfa7
Mathiprakash123 <mathisivaprakash141@gmail.com>
2024-06-03 13:43:03 +0530

    after rename exp-3

--- exp-3/exp.java
+++ exp-3/exp.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BitStuffing {
+public class exp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the message:");
